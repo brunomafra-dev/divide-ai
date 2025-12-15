0add86152c9676e100adab57b85b405bc0c729d7
Bruno Mafra <brunolamafra@hotmail.com>
2025-12-15 10:11:16 -0300

    Update ProtectedNavbar.tsx

--- src/components/ProtectedNavbar.tsx
+++ src/components/ProtectedNavbar.tsx
@@ -1,1 +1,14 @@
+'use client'
+
+import { useAuth } from '@/context/AuthContext'
+import Navbar from '@/components/Navbar'
+
+export default function ProtectedNavbar() {
+  const { user, loading } = useAuth()
+
+  if (loading) return null
+  if (!user) return null
+
+  return <Navbar />
+}
 
