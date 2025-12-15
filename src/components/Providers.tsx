c03024db25d4d6c1db2b0c8d1f6f5d25d3276190
Bruno Mafra <brunolamafra@hotmail.com>
2025-12-15 11:00:22 -0300

    Update Providers.tsx

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,1 +1,21 @@
+'use client'
+
+import Navbar from "@/components/Navbar";
+import ActionModalProvider from "@/components/ActionModalProvider";
+import { AuthProvider } from "@/context/AuthContext";
+
+export default function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <AuthProvider>
+      <ActionModalProvider>
+        <div className="pb-32">{children}</div>
+        <Navbar />
+      </ActionModalProvider>
+    </AuthProvider>
+  );
+}
 
